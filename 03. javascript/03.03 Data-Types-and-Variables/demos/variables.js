4f6fe02899a2382c0f8e2ebadffd537f3b36fc10
Napoleon-Alexandru Sireteanu <alexandrusireteanu@users.noreply.github.com>
2023-05-04 12:22:35 +0300

    Add files via upload

---
+++ 03. javascript/03.03 Data-Types-and-Variables/demos/variables.js
@@ -0,0 +1,12 @@
+// In order to declare variables we need to write var keywords. This was the engine that it needs to reserve some memory in which to store your data later. When we declare the variable javascript engine understands that it has to create memory space in which you can store the data
+// After that you should set the name to the variable. Javascript is a weekly type language and you don't need to limit yourself to what type of data the variable can hold. It means that javascript engine understands itself what type of data you are storing in the variable.
+
+var person = 'instructor';
+console.log(person);
+
+person = 'teacher';
+console.log(person);
+
+var firstName = 'John', 
+    lastName = 'Doe', 
+    age = 27;
